efabf8696f64362a19dbf6432b165911f2c522f3
jonz94 <jody16888@gmail.com>
2021-12-01 14:25:38 +0800

    🚧 WIP: work in progress...

---
+++ 0-main.ps1
@@ -0,0 +1,7 @@
+Get-ChildItem bucket | ForEach-Object {
+    # patch
+    node "1-patch.mjs" $_.FullName
+
+    # add `"depends": "scoop-nerd-fonts-helper",` part, also sort the order of the properties
+    node "2-add-depends-and-sort.mjs" $_.FullName
+}
